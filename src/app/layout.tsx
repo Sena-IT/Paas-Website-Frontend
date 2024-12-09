42950f870fdc0daeb47900c971f70a922286a2e3
naveen <naveenmk675@gmail.com>
2024-12-09 13:02:07 +0530

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <link rel="icon" href="/senaYellowFlag.svg" />
           <link
           href="https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap"
           rel="stylesheet"
